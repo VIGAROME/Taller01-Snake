868f0cd5dacd33e54a054b70d97d7759f017aff7
Victor Garcia <vigarome@espol.edu.ec>
2022-10-05 16:53:04 -0500

    Cambio lider 2 intento

--- src/lab1/GUIView.java
+++ src/lab1/GUIView.java
@@ -68,7 +68,7 @@ public class GUIView extends JPanel {
 
 		// Create a new button on that panel and add a StartGameListener as
 		// listener on that button
-		this.startGameButton = new JButton("Start Game");
+		this.startGameButton = new JButton("Jugar");
 		this.startGameButton.addActionListener(new StartGameListener());
 		this.guiPanel.add(this.startGameButton);
 
